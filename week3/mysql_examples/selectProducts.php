0eca2c4bbe89724cdec4ba2a61cb298705bfcfae
Mark Nikora <itmtn@internal.rexnet.nz>
2019-08-06 09:51:35 +1200

    product add to cart code initial example added

--- week3/mysql_examples/selectProducts.php
+++ week3/mysql_examples/selectProducts.php
@@ -8,10 +8,15 @@ $productsHTML = "";
 while($row = $result->fetch_array(MYSQLI_NUM)){
 $productsHTML .= <<<EOD
   <div class="product">
-    <p>$row[0]</p>
+    <p><a href="addToCart.php?product_no=$row[0]">$row[0]</a></p>
     <p>$row[1]</p>
     <p>$row[2]</p>
     <p>$row[3]</p>
+    <form action="addToCart.php" method="post">
+    <input type="hidden" name="pid" value="$row[0]">
+    <input type="number" name="amount" value="0">
+    <input type="submit" value="add to cart"> 
+    </form>
   </div>
 EOD;
 }
