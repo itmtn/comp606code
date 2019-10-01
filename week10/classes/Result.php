5bbd82f1f48a42d72d1cc31c14d67a2bde0c6034
Mark Nikora <itmtn@internal.rexnet.nz>
2019-10-02 09:48:12 +1300

    shell for result class

--- week10/classes/Result.php
+++ week10/classes/Result.php
@@ -3,15 +3,31 @@
 
 class Result {
 
+    private $id = "";
     private $studentId = "";
     private $assessmentId = "";
     private $score = 0;
     private $grade = "";
 
+    public function __construct($id, $studentId, $assessmentId, $score, $grade){        
+    }
+
+    // helper methods
+    public static function create($studentId, $assessmentId, $score, $grade){        
+    }
+
+    public static function find($mysqli, $id){
+
+    }
+
+    public static function getAll($mysqli){
+        
+    }
+
+    // getters
 
 
-
-
+    // setters
 
 
 }
